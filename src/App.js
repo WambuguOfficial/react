e1ff80f2e72b0abf90bccf81f7b5ab0f56ec0ea6
WambuguOfficial <johnwambugu710@gmail.com>
2023-02-02 08:59:35 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const sunsets = [
+    { id: 1, src: 'C:\Users\user\my-app\sunset_files\beautiful-sunset-tropical-beach-palm-260nw-1716193708.jpg' },
+    { id: 2, src: 'C:\Users\user\my-app\download.jpeg' },
+    { id: 3, src: 'C:\Users\user\my-app\sunsetpic.jpg' },
+    { id: 4, src: 'C:\Users\user\my-app\sunsetpic.jpg' },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Best Sunset Pictures</h1>
+      <div className="photo-grid">
+        {sunsets.map(sunset => (
+          <img key={sunset.id} src={sunset.src} alt="Sunset" />
+        ))}
+      </div>
     </div>
   );
 }
